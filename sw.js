c03e8bab85367319f06307f43d173ea02ce40fc5
tricgrizen <1299750109@qq.com>
2026-08-23 20:31:29 -0400

    batch3: ten new root families (ject stru cur clud serv spond cid fin solv pel), 33 roots total, 37 content files 0E0W, selftest 19/19, build 00c0ca15

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 铸影 sw.js（build_vocab.py 产）：版本化预缓存；数据件独立缓存名（内容不变不重下）。*/
-var V="vc-59175e15", DATA="vc-data-7887aaf2";
+var V="vc-59175e15", DATA="vc-data-1e2fcbe0";
 var PRE=["./","./index.html","./vocab_sync.js","./manifest.webmanifest","./icon-512.png","./icon-192.png","./icon-180.png"];
 self.addEventListener("install",function(e){e.waitUntil(caches.open(V).then(function(c){return c.addAll(PRE);}).then(function(){return self.skipWaiting();}));});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==V&&k!==DATA;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
